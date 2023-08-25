5bf7bf350fdbbab68daf20b4fb4ab2e7b9d98df5
Greger Stolt Nilsen <gregersn@gmail.com>
2023-08-25 22:46:20 +0200

    Use pyproject.toml and hatchling to build wheel.
    Build package with python -m build

--- setup.py
+++ setup.py
@@ -1,14 +1,3 @@
-from setuptools import setup, find_packages
-setup(
-    name='imagediff',
-    version='1.0.0.dev1',
-    description='A utility to compare folders of images',
-    url='TBD',
-    author='Greger Stolt Nilsen',
-    author_email='dev@gregerstoltnilsen.net',
-    license='MIT',
-    packages=find_packages(include=['imagediff']),
-    install_requires=['Pillow>=9.0.1', 'PySide6>=6.3.2', 'Click>=8.0.1'],
-    python_requires='>=3',
-    scripts=['bin/imagediff']
-)
+from setuptools import setup
+
+setup()
